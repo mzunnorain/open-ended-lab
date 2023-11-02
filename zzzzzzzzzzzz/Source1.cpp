5d14bfe5634e559f56680be230a20e30ed782a32
Muhammad Zunnorain Ali <m.zunnorain.786@gmail.com>
2023-11-02 09:22:27 -0700

    part 2

--- zzzzzzzzzzzz/Source1.cpp
+++ zzzzzzzzzzzz/Source1.cpp
@@ -10,18 +10,40 @@ int main()
 	cout << "Enter your choice";
 	cin >> choice;
 	switch(choice)
+        case'1': 
+            
+            
+            
+            
+            break;
+
+
+
+
+
+
+
+
      case'2':
-		 
-		 cout << "Enter the number you want to check : ";
-		 cin >> number;
 
-		 for (int i = 1; i <= number; i++)
+         int number, count;
 
-		 {
-			 if (number % i == 0)
-			 {
-				 cout << "Not a prime number ";
-			 }
-		 }
+         cout << "Enter the number: ";
+         cin >> number;
+         count = 0;// count keeps record of factors of the number 
+         for (int i = 1; i <= number; i++)
+         {
+             if (number % i == 0)
+             {
+                 count++;
+             }
+         }
+         if (count == 2)//since prime number has onnly 2 factors ,1 and the number itself.
+             cout << "Prime number";
+         else
+             cout << "Not a prime number ";
+         break;
+
+		
 
 }
